d3477c33ea7c881a61f3fbf13d1559f5300049ce
Jérôme.Vouillon <jvouillon@besport.com>
2020-10-19 15:23:33 +0200

    Swith to ppxlib

--- i18n_ppx_rewriter.ml
+++ i18n_ppx_rewriter.ml
@@ -16,22 +16,20 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 *)
 
-open Ast_mapper
-open Asttypes
-open Parsetree
-open Longident
+open! Ppxlib
 
 let default_module_name = ref ""
 let module_prefix = ref ""
 let module_suffix = ref ""
 
-let mk_ident module_name_o i =
+let mk_ident i =
+  if !default_module_name = "" then invalid_arg "Missing default module name";
   let module_name, safe_ident =
-    match i.txt with
+    match i with
     (* CASE 1: [%i18n ident ... ] *)
-    | Lident _ -> !default_module_name, i.txt
+    | Lident _ -> !default_module_name, i
     (* CASE 2: [%i18n S.ident ... ] *)
-    | Ldot (Lident "S", _) -> !default_module_name, i.txt
+    | Ldot (Lident "S", _) -> !default_module_name, i
     (* CASE 3: [%i18n OtherMod.ident ...] *)
     | Ldot (Lident module_name, s) ->
      !module_prefix ^ module_name ^ !module_suffix, Lident s
@@ -40,51 +38,66 @@ let mk_ident module_name_o i =
      !module_prefix ^ module_name ^ !module_suffix, Ldot (Lident "S", s)
     (* CASE X : [%i18n OtherModA.OtherModB.ident ...]. Illegal. *)
     | i -> let err_msg =
-             Format.asprintf "%a is not a valid i18n expression"
-               Pprintast.longident i
+             Format.asprintf "%s is not a valid i18n expression"
+               (Longident.name i)
       in failwith err_msg
   in
-  match
-    Longident.unflatten (module_name :: "Tr" :: Longident.flatten safe_ident)
-  with
-  | None   -> assert false
-  | Some i -> i
+  let rec prefix i =
+    match i with
+    | Lident id ->
+       Ldot (Ldot (Lident module_name, "Tr"), id)
+    | Ldot (i, nm) ->
+       Ldot (prefix i, nm)
+    | Lapply _ ->
+       let err_msg =
+             Format.asprintf "%s is not a valid i18n expression"
+               (Longident.name i)
+      in failwith err_msg
+  in
+  prefix safe_ident
 
-let unit loc = [%expr ()]
+let unit loc =
+  let open Ast_builder.Default in
+  (Nolabel, pexp_construct ~loc (Located.mk ~loc (Lident "()")) None)
 
 let lang_args = function
   | ((Labelled("?lang"), _) as lang) :: args
   | ((Labelled("~lang"), _) as lang) :: args -> ([ lang ], args)
   | x -> ([], x)
 
-let apply e args =
+let ident loc id =
+  let open Ast_builder.Default in
+  pexp_ident ~loc (Located.mk ~loc (mk_ident id))
+
+let apply loc id args =
   let (lang, args) = lang_args args in
-  Pexp_apply (e, lang @ [ ( Nolabel, unit e.pexp_loc ) ]
-                 @ args @ [ ( Nolabel, unit e.pexp_loc ) ] )
+  let open Ast_builder.Default in
+  pexp_apply ~loc (ident loc id) (lang @ [unit loc] @ args @ [unit loc])
 
-let ident module_name_o expr e i =
-  let e' =
-    { e with pexp_desc = Pexp_ident { i with txt = mk_ident module_name_o i } }
-  in
-  { expr with pexp_desc = apply e' [] }
+let () =
+  List.iter
+    (fun (key, spec, doc) -> Ppxlib.Driver.add_arg key spec ~doc)
+    ["--prefix", Arg.Set_string module_prefix,
+     "PREFIX The prefix added to module names"
+    ;"--suffix", Arg.Set_string module_suffix,
+     "SUFFIX The suffix added to module names"
+    ;"--default-module", Arg.Set_string default_module_name,
+     "MODULE Name of the default module"]
 
-let apply module_name_o expr e i args =
-  let e' =
-    { e with pexp_desc = Pexp_ident { i with txt = mk_ident module_name_o i } }
-  in
-  { expr with pexp_desc = apply e' args }
+let expand ~loc:loc0 ~path:_ expr =
+  Ppxlib.Ast_pattern.
+  (parse (pexp_ident __ |> map1 ~f:(fun id -> apply loc0 id [])
+          ||| (pexp_apply (pexp_ident __) (many __)  |> map2 ~f:(apply loc0))))
+    loc0 expr (fun x -> x)
 
-(* Usage: -ppx "i18n_ppx_rewrite.native Module_name" *)
-let options_spec =
-  ["--prefix", Arg.Set_string module_prefix, "The prefix added to module names"
-  ;"--suffix", Arg.Set_string module_suffix, "The suffix added to module names"
-  ;"--default-module", Arg.Set_string default_module_name,
-   "Name of the default module"]
+let extension =
+  Ppxlib.Extension.declare
+    "i18n"
+    Ppxlib.Extension.Context.expression
+    Ppxlib.Ast_pattern.(single_expr_payload __)
+    expand
 
-let _ =
-  Migrate_parsetree.Driver.register ~name:"i18n" ~args:options_spec
-    Migrate_parsetree.Versions.ocaml_411
-    (fun _config _cookies ->
-      if !default_module_name = "" then invalid_arg "Missing default module name";
-      { default_mapper
-        with expr = I18n_ppx_common.mkmapper default_mapper ident apply  })
+let rule = Ppxlib.Context_free.Rule.extension extension
+
+let () =
+  Ppxlib.Driver.register_transformation ~rules:[rule] "i18n"
